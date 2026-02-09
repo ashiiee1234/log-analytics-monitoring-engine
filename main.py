d2f012125df8994959a8dafd3e722f2b2843aa5c
ashiiee1234 <132062348+ashiiee1234@users.noreply.github.com>
2026-02-10 01:39:30 +0530

    Clean up commented-out code in main.py
    Removed commented-out code related to user email and anomaly detection.

--- main.py
+++ main.py
@@ -28,47 +28,7 @@ app.add_middleware(
 )
 
 
-# user_mail = "rohanbelsare113@gmail.com"
-# def main():
-    # client = start_dask()
-    # print(client)
-    # print(f"Dashboard link: {client.dashboard_link}")
-    # print("\n" + "=" * 50)
-
-    # start = time.time()
-    # log_df = build_pipeline(r"data\sample_log.log")
-    # print(log_df.tail())
-    # print("start time:", start)
-
-    # total_logs = log_df.count().compute()
-    # end = time.time()
-
-    # print("Running Anomaly detection...")
-    # anomalies_df = detect_anomaly(log_df)
-
-    # # anomalies = anomalies_df.compute()
-    # anomalies = anomalies_df
-
-    # if anomalies.empty:
-    #     print("No anomalies detected")
-    # else:
-    #     print(f"🚨 {len(anomalies)} anomalies detected!")
-
-    #     for _, row in anomalies.iterrows():
-    #         anomaly_data = {
-    #             "timestamp": row["timestamp"],
-    #             "error_count": row["error_count"],
-    #             "z_score": row["z_score"],
-    #         }
-
-    #         send_mail(to_email=user_mail, anomaly=anomaly_data)
-
-    #         print(
-    #             f"📧 Alert sent | Time: {row['timestamp']} | "
-    #             f"Errors: {row['error_count']}"
-    #         )
-
-    # input("\nPress Enter to exit...")
+]
 
 app.include_router(service.router)
 # if __name__ == "__main__":
