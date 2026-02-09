c91b39ee337e68f8f38136e8229dbceef46145b3
ashiiee1234 <132062348+ashiiee1234@users.noreply.github.com>
2026-02-10 01:36:11 +0530

    Update sql_config.py

--- backend/config/sql_config.py
+++ backend/config/sql_config.py
@@ -6,7 +6,7 @@ def connect_sql():
         connection = mysql.connector.connect(
         host="localhost",
         user="root",
-        password="Rohan@54321",
+        password="Ashi@54321",
         database="springboard",
         port=3306
     )
